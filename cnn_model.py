2ee78219733034a1f61988be76a12234a35bcadd
deco <deco@cubee.com>
2018-05-21 18:41:19 +0800

    Prepare data

--- cnn_model.py
+++ cnn_model.py
@@ -7,19 +7,24 @@ class TCNNConfig(object):
     """CNN配置参数"""
 
     embedding_dim = 64  # 词向量维度
-    seq_length = 600  # 序列长度
-    num_classes = 10  # 类别数
+    seq_length = 100  # 序列长度
+    # seq_length = 600
+    num_classes = 20  # 类别数
+    # num_classes = 10
     num_filters = 256  # 卷积核数目
     kernel_size = 5  # 卷积核尺寸
-    vocab_size = 5000  # 词汇表达小
+    vocab_size = 843  # 词汇表大小
+    # vocab_size = 5000
 
-    hidden_dim = 128  # 全连接层神经元
+    hidden_dim = 16  # 全连接层神经元
+    # hidden_dim = 128
 
     dropout_keep_prob = 0.5  # dropout保留比例
     learning_rate = 1e-3  # 学习率
 
     batch_size = 64  # 每批训练大小
-    num_epochs = 10  # 总迭代轮次
+    num_epochs = 1  # 总迭代轮次
+    # num_epochs = 10
 
     print_per_batch = 100  # 每多少轮输出一次结果
     save_per_batch = 10  # 每多少轮存入tensorboard
