c06ceff8e2626829f15c6155288f1dd81fb73940
deco <deco@cubee.com>
2018-06-21 18:32:55 +0800

    es先filter一个条件再aggs

--- elasticsearch2/add_dongqiudi.py
+++ elasticsearch2/add_dongqiudi.py
@@ -45,6 +45,27 @@ def set_logger():
     return logger
 
 
+def create_mapping():
+    setting = {
+        "mappings": {
+            "news": {
+                "properties": {
+                    "date": {
+                        "type": "date",
+                        "format": "YYYY-MM-DD HH:mm:ss"
+                    },
+                    "display_time": {
+                        "type": "date",
+                        "format": "YYYY-MM-DD HH:mm:ss"
+                    }
+                }
+            }
+        }
+    }
+
+    es.indices.create(index='dongqiudi', body=setting)
+
+
 def put_data(tpl_intent_entity):
     i = 1
     for tie in tpl_intent_entity:
@@ -58,12 +79,16 @@ def put_data(tpl_intent_entity):
 def set_data(list_data_dict, index_name="dongqiudi", doc_type_name="news"):
     """https://github.com/elastic/elasticsearch-py/issues/508"""
 
+    unique_set = set()
+
     for data_dict in list_data_dict:
-        yield {
-            "_index": index_name,
-            "_type": doc_type_name,
-            "_source": data_dict
-        }
+        if data_dict['title'][0] not in unique_set:
+            unique_set.add(data_dict['title'][0])
+            yield {
+                "_index": index_name,
+                "_type": doc_type_name,
+                "_source": data_dict
+            }
 
     # for idx, data_dict in enumerate(list_data_dict):
     #     yield {
@@ -147,10 +172,60 @@ def search_data_match_all():
 def search_data_match():
     p = es.search(index="dongqiudi",
                   doc_type="news",
-                  body={"_source": ["title", "description"],
+                  body={"_source": ["title", "description", "display_time"],
                         "query": {"bool": {
                          "must": {"match": {"title": "世界杯"}},
                         }}})
+    pprint.pprint(p)
+
+
+def search_data_term():
+    p = es.search(index="dongqiudi",
+                  doc_type="news",
+                  body={
+                        "query": {"bool": {"filter": {"bool": {
+                         "must": {"term": {"title.keyword":
+                          "7分钟透析俄罗斯世界杯的“中国队” | B面世界杯"}},
+                        }}}}})
+    pprint.pprint(p)
+
+
+def search_data_date():
+    p = es.search(index="dongqiudi",
+                  doc_type="news",
+                  body={
+                        "query": {"constant_score": {"filter": {
+                            "range": {"display_time": {
+                                "gte": '2018-06-20 00:00:00',
+                                }},
+                            }}}})
+    pprint.pprint(p)
+
+
+def search_data_date2():
+    p = es.search(index="dongqiudi",
+                  doc_type="news",
+                  body={
+                        "query": {"bool": {"filter": {
+                            "range": {"display_time": {
+                                    "gte": '2018-06-20',
+                                    "format": "YYYY-MM-DD"
+                                    # "format": "yyyy-MM-dd"
+                                    }}
+                            }}}})
+    pprint.pprint(p)
+
+
+def search_data_date3():
+    p = es.search(index="dongqiudi",
+                  doc_type="news",
+                  body={
+                        "query": {"bool": {"filter": {
+                            "range": {"display_time": {
+                                    "gte": '2018-06-20 00:00:00',
+                                    "lte": '2018-06-21 00:00:00',
+                                    }}
+                            }}}})
     pprint.pprint(p)
 
 
@@ -181,6 +256,8 @@ def sentence2tags(st):
                                           'eng', 'n', 'ns', 'nr', 'nt', 'nz',
                                           'vn', 'vi', 'vq'))
     # 一方面，用tf-idf提取句中关键词，另一方面，只取句中的英文、名词和动词
+    stop_words = {'全场', '出线', '球帝', '图集', '代表', '世界', '足球'}
+    tags = [tag for tag in tags if tag not in stop_words]
     return tags
 
 
@@ -218,21 +295,77 @@ def word_cloud():
     pprint.pprint(p['aggregations'])
 
 
+def word_cloud2():
+    p = es.search(index="dongqiudi",
+                  doc_type="news",
+                  body={
+                        "query": {"bool": {"filter": {
+                            "range": {"display_time": {
+                                "gte": '2018-06-20',
+                                "lte": '2018-06-20',
+                                "format": "YYYY-MM-DD"}},
+                            }}},
+
+                        "aggs": {
+                            "wordcloud": {
+                                "terms": {
+                                    "field": "key_tags.keyword",
+                                    "size": 20
+                                }
+                            }
+                        },
+                    })
+    pprint.pprint(p['aggregations'])
+
+
+def word_cloud3():
+    p = es.search(index="dongqiudi",
+                  doc_type="news",
+                  body={
+                        "query": {"bool": {"filter": {
+                            "range": {"display_time": {
+                                "gte": '2018-06-21 00:00:00',
+                                "lte": '2018-06-22 00:00:00',
+                                }},
+                            }}},
+
+                        "aggs": {
+                            "wordcloud": {
+                                "terms": {
+                                    "field": "key_tags.keyword",
+                                    "size": 20
+                                }
+                            }
+                        },
+                    })
+    pprint.pprint(p['aggregations'])
+
+
 if __name__ == '__main__':
     # del_dongqiudi()
+
+    # create_mapping()
 
     # dict2es()
 
     # search_data_size()
 
-    # count_data()
+    # view_mapping()
+
+    # search_data_term()
+
+    # ***********************************************
 
     # append_data()
 
     # update_key_tags()
 
-    # view_mapping()
+    # count_data()
 
     # word_cloud()
 
-    search_data_match()
+    # search_data_match()
+
+    # word_cloud2()
+
+    word_cloud3()
